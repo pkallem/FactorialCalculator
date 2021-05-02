d094c1f1b0a53b3dc4773057fe41c6d267d03086
Pranav Kallem <66687154+pkallem@users.noreply.github.com>
2021-05-02 13:25:51 -0400

    Add files via upload

---
+++ Form1-9.vb
@@ -0,0 +1,14 @@
+﻿Public Class frmFactorial
+
+
+
+    Private Sub btnButton_Click(sender As Object, e As EventArgs) Handles btnButton.Click
+        Dim intFact As Integer = 1
+        Dim intNum As Integer = InputBox("Enter your number", "Factorial")
+        For intNum = intNum To 1 Step -1
+            intFact = intNum * intFact
+        Next
+        lblLabel.Text = intFact
+
+    End Sub
+End Class
